9ccd8053bcebfaa0e60c4c5b0587e9d55863cfdb
pdw1113 <drnew0313@gmail.com>
2024-08-30 16:55:55 +0900

    주석 추가

--- websocket/src/main/java/com/example/websocket/chat/ChatController.java
+++ websocket/src/main/java/com/example/websocket/chat/ChatController.java
@@ -19,14 +19,21 @@ public class ChatController {
 	private final SimpMessagingTemplate messagingTemplate;
 	private final ChatMessageService chatMessageService;
 	
+	/**
+	 * convertAndSendToUser(String user, String destination, Object payload)
+	 * >> 채널에 구독하고 있는 사용자들 중 모두에게가 아닌 특정한 사용자에게 메세지를 보낼 수 있도록 해주는 메소드
+	 * 
+	 * 
+	 * @param chatMessage
+	 */
 	@MessageMapping("/chat")
 	public void processMessage(@Payload ChatMessage chatMessage) {
 		ChatMessage savedMsg = chatMessageService.save(chatMessage);
 		// ex : john/queue/messages
 		messagingTemplate.convertAndSendToUser(
-				chatMessage.getRecipientId(), 
-				"/queue/messages", 
-				ChatNotification.builder()
+				chatMessage.getRecipientId()
+				, "/queue/messages"
+				, ChatNotification.builder()
 					.id(savedMsg.getId())
 					.senderId(savedMsg.getSenderId())
 					.recipientId(savedMsg.getRecipientId())
